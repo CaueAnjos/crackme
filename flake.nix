7eaadbb0f0686f730eca2ddfd594586b1da05593
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-04-24 23:36:17 -0300

    feat(flake): init flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A bundle of crackmes to crack and test";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.11";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        (import ./packages {inherit inputs;})
+        ./scripts
+      ];
+      systems = import inputs.systems;
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          name = "dev";
+          packages = with pkgs; [
+            cowsay
+          ];
+        };
+      };
+    };
+}
