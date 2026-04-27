7be6faa60effc24cb81ae4f9ea6b75e9bd562545
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-04-27 11:19:12 -0300

    feat: add lua

--- src/main.c
+++ src/main.c
@@ -1,32 +1,18 @@
-#include <math.h>
-#include <stdint.h>
-#include <stdio.h>
+#include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
 
-double_t generate_password(double_t input);
-
-typedef uint8_t bool;
-
-int main(int argc, char *argv[]) {
-  const char world[] = "World";
-  puts("Give me a number");
-
-  int32_t n = 0;
-  int32_t r = scanf("%d", &n);
-
-  switch (n) {
-  case 1:
-    puts("1");
-    break;
-  case 2:
-    puts("2");
-    break;
-  case 3:
-    puts("3");
-    break;
-  default:
-    puts("bad");
-    break;
-  }
-
+int my_add(lua_State *L) {
+  int a = luaL_checkinteger(L, 1);
+  int b = luaL_checkinteger(L, 2);
+  lua_pushinteger(L, a + b);
+  return 1;
+}
+int main() {
+  lua_State *L = luaL_newstate();
+  luaL_openlibs(L);
+  lua_register(L, "add", my_add);
+  luaL_dostring(L, "print(add(10, 20))");
+  lua_close(L);
   return 0;
 }
