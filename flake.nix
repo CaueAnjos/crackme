8976a66174c43fcf31fa0d21b6ca47b1609213c3
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2026-04-30 13:56:08 -0300

    feat: add proper minhook lib

--- flake.nix
+++ flake.nix
@@ -50,6 +50,7 @@
             echo "entering windows dev env!"
             CC="x86_64-w64-mingw32-gcc"
             CXX="x86_64-w64-mingw32-g++"
+            AR="x86_64-w64-mingw32-ar"
           '';
           env = {
             PKGCONFIG = "x86_64-w64-mingw32-pkg-config";
